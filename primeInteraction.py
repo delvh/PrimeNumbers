dd346f78287f2919d8eb0f0a669002e50a699994
delvh <dev.lh@web.de>
2020-01-29 14:59:09 +0100

    added square root optimisation
    Signed-off-by: delvh <dev.lh@web.de>

--- primeInteraction.py
+++ primeInteraction.py
@@ -1,6 +1,26 @@
 import time
+from math import sqrt
 
 primes = [1]  # 1 is excluded of being a prime number
+maximumPrimeAdded = -1
+restrictPrimes = True
+
+
+def reset():
+    global primes, maximumPrimeAdded, restrictPrimes
+    primes = [1]
+    maximumPrimeAdded = -1
+    restrictPrimes = True
+
+
+def setMaxPrimes(maximum):
+    global maximumPrimeAdded
+    maximumPrimeAdded = sqrt(maximum)
+
+
+def setRestricted(boolean):
+    global restrictPrimes
+    restrictPrimes = boolean
 
 
 def getDivisors(value):
@@ -14,16 +34,21 @@ def getDivisors(value):
 def checkIfPrime(value):  # Checks if a number is prime
     currentDivisors = getDivisors(value)
     if currentDivisors == 1:
-        primes.append(value)
+        global restrictPrimes, maximumPrimeAdded
+        if restrictPrimes and value < maximumPrimeAdded:
+            primes.append(value)
         return True
     return False
 
 
 def numberOfPrimes(maximum):  # Checks how many primes are in a given range
     amountOfPrimes = 0
+    setRestricted(True)
+    setMaxPrimes(maximum)
     for i in range(2, maximum):
         if checkIfPrime(i):
             amountOfPrimes += 1
+    reset()
     return amountOfPrimes
 
 
